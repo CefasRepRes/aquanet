5f219f5642102c2640aa6370d6106e4ee2415afe
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-06 14:47:36 +0100

    inherited code initial commit ListSiteDetailswithModelID

---
+++ R/ListSiteDetailsWithModelID.R
@@ -0,0 +1,21 @@
+ListSiteDetailsWithModelID = function(graph_full, graph.contactp.objects, siteLocationsWithCatchment.fileName) {
+  siteID = V(graph_full)$siteID
+  personID = V(graph_full)$PersonID
+  modelID = 1:length(siteID)
+  siteName = V(graph_full)$siteName
+  catchmentID = V(graph_full)$catchmentID
+  type = V(graph_full)$type
+  
+  siteDetailsWithModelID = data.frame(siteID, personID, modelID, siteName, catchmentID, type)
+  
+  site2Catchment.table = read.csv(file = siteLocationsWithCatchment.fileName, header = TRUE)
+  
+  siteDetailsWithModelID.withCatchmentDetails = merge(x = siteDetailsWithModelID,
+                                                      y = site2Catchment.table,
+                                                      by = c('siteID'),
+                                                      all.x = TRUE)
+  
+  siteDetailsWithModelID.withCatchmentDetails = siteDetailsWithModelID.withCatchmentDetails[order(siteDetailsWithModelID.withCatchmentDetails$modelID),]  
+
+  return(siteDetailsWithModelID.withCatchmentDetails)
+}
