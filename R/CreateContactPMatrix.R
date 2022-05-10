cdbf6a14347423b0e67fa22d6bd92a1bf031bb14
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-10 13:25:57 +0100

    rename parameter contactp to matrix_movements in CreateContactPMatrix()

--- R/CreateContactPMatrix.R
+++ R/CreateContactPMatrix.R
@@ -1,12 +1,12 @@
 CreateContactPMatrix <- function(graph, movement_period) {
   # get sparse adjacency matrix of movements between sites from contact network (graph)
-  contactp <- igraph::get.adjacency(graph,
+  matrix_movements <- igraph::get.adjacency(graph,
                                     attr = "movements",
                                     names  = TRUE,
                                     sparse = TRUE)
 
   # divide number of movements by the time over which movement information was collected
-  contactp.prob <- contactp/movement_period
+  contactp.prob <- matrix_movements/movement_period
 
   # if there are any contacts with a probability of greater than one, assume a probability of one
   contactp.prob[contactp.prob > 1] <- 1
@@ -19,5 +19,5 @@ CreateContactPMatrix <- function(graph, movement_period) {
 
   # return list containing number of sites in contact matrix, adjacency matrix of movements between sites,
   # and probability matrix of movements between sites
-  return(list(LengthContactP, contactp, contactp.prob))
+  return(list(LengthContactP, matrix_movements, contactp.prob))
 }
