38cf6f1ad8dd5a70bc97cbe655ca83307d33c560
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-10 13:27:01 +0100

    rename parameter contactp.prob to matrix_movements_prob in CreateContactPMatrix()

--- R/CreateContactPMatrix.R
+++ R/CreateContactPMatrix.R
@@ -6,18 +6,18 @@ CreateContactPMatrix <- function(graph, movement_period) {
                                     sparse = TRUE)
 
   # divide number of movements by the time over which movement information was collected
-  contactp.prob <- matrix_movements/movement_period
+  matrix_movements_prob <- matrix_movements/movement_period
 
   # if there are any contacts with a probability of greater than one, assume a probability of one
-  contactp.prob[contactp.prob > 1] <- 1
+  matrix_movements_prob[matrix_movements_prob > 1] <- 1
 
   # uncompress 'dgCMatrix' to 'dgTMatrix' type (easier to look up the source of infection)
-  contactp.prob <- methods::as(contactp.prob, 'dgTMatrix')
+  matrix_movements_prob <- methods::as(matrix_movements_prob, 'dgTMatrix')
 
   # extract number of sites represented within the model
-  LengthContactP <- length(contactp.prob[, 1])
+  LengthContactP <- length(matrix_movements_prob[, 1])
 
   # return list containing number of sites in contact matrix, adjacency matrix of movements between sites,
   # and probability matrix of movements between sites
-  return(list(LengthContactP, matrix_movements, contactp.prob))
+  return(list(LengthContactP, matrix_movements, matrix_movements_prob))
 }
