b228fcd02ffe07f722fad501195151acd5f7814a
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-10 13:13:07 +0100

    clarify comments in function CreateContactPMatrix

--- R/CreateContactPMatrix.R
+++ R/CreateContactPMatrix.R
@@ -1,21 +1,23 @@
 CreateContactPMatrix <- function(graph, periodDataCollection) {
-  # Convert the contact network to a matrix, recording the number of movements that occur between sites
+  # get sparse adjacency matrix of movements between sites from contact network (graph)
   contactp <- igraph::get.adjacency(graph,
                                     attr = "movements",
                                     names  = TRUE,
                                     sparse = TRUE)
 
-  # Divide the number of movements by the time over which information was collected
+  # divide number of movements by the time over which movement information was collected
   contactp.prob <- contactp/periodDataCollection
 
-  # If there are any contacts with a probability of greater than one, assume a probability of one
+  # if there are any contacts with a probability of greater than one, assume a probability of one
   contactp.prob[contactp.prob > 1] <- 1
 
-  # Convert the matrix to 'dgTMatrix' type, so it is easier to lookup the source of infection
+  # uncompress 'dgCMatrix' to 'dgTMatrix' type (easier to look up the source of infection)
   contactp.prob <- methods::as(contactp.prob, 'dgTMatrix')
 
-  # Check the final number of sites represented within the model
+  # extract number of sites represented within the model
   LengthContactP <- length(contactp.prob[, 1])
 
+  # return list containing number of sites in contact matrix, adjacency matrix of movements between sites,
+  # and probability matrix of movements between sites
   return(list(LengthContactP, contactp, contactp.prob))
 }
