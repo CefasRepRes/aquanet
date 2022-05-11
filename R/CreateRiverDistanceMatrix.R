867670b6cd6ba9c5a1e0f5dcdb53d6c1156563bf
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 12:14:14 +0100

    change = to <- assignment operator to define functions and variables in CreateRiverDistanceMatrix()

--- R/CreateRiverDistanceMatrix.R
+++ R/CreateRiverDistanceMatrix.R
@@ -1,30 +1,30 @@
-CreateRiverDistanceMatrix = function(riverDistance.fileName, graph.contactp.objects, ListModelSetupParameters, TypeRiverTransmission) {
+CreateRiverDistanceMatrix <- function(riverDistance.fileName, graph.contactp.objects, ListModelSetupParameters, TypeRiverTransmission) {
   # List of sites, in the same order as the adjacency matrix
-  graph.contactp.listSites = as.numeric(graph.contactp.objects[[3]]@Dimnames[[1]])
+  graph.contactp.listSites <- as.numeric(graph.contactp.objects[[3]]@Dimnames[[1]])
 
   # Store the number of sites in the contactp matrix
-  contactp.length = graph.contactp.objects[[1]]
+  contactp.length <- graph.contactp.objects[[1]]
 
-  River.Transmission_Const = 1 / as.numeric(ListModelSetupParameters[c('River.Downstream_Transmission_Const','River.UpDownstream_Transmission_Const')])
-  River.Transmission_Current_Speed = as.numeric(ListModelSetupParameters[c('River.Downstream.Current_speed','River.UpDownstream.Current_speed')])
+  River.Transmission_Const <- 1 / as.numeric(ListModelSetupParameters[c('River.Downstream_Transmission_Const','River.UpDownstream_Transmission_Const')])
+  River.Transmission_Current_Speed <- as.numeric(ListModelSetupParameters[c('River.Downstream.Current_speed','River.UpDownstream.Current_speed')])
 
   # Import the table of site to site distances, through the river network
-  riverDistance.table = read.csv(file = riverDistance.fileName, stringsAsFactors = FALSE)
+  riverDistance.table <- read.csv(file = riverDistance.fileName, stringsAsFactors = FALSE)
 
-  riverDistance.table$Origin.SiteID = regmatches(x = riverDistance.table$Name, m = regexpr(perl = TRUE, text = riverDistance.table$Name, pattern = "^[0-9]+"))
-  riverDistance.table$Dest.SiteID = regmatches(x = riverDistance.table$Name, m = regexpr(perl = TRUE, text = riverDistance.table$Name, pattern = "[0-9]+$"))
+  riverDistance.table$Origin.SiteID <- regmatches(x = riverDistance.table$Name, m = regexpr(perl = TRUE, text = riverDistance.table$Name, pattern = "^[0-9]+"))
+  riverDistance.table$Dest.SiteID <- regmatches(x = riverDistance.table$Name, m = regexpr(perl = TRUE, text = riverDistance.table$Name, pattern = "[0-9]+$"))
 
-  riverDistance.table = subset(riverDistance.table, Origin.SiteID %in% as.character(graph.contactp.listSites) & Dest.SiteID %in% as.character(graph.contactp.listSites))
+  riverDistance.table <- subset(riverDistance.table, Origin.SiteID %in% as.character(graph.contactp.listSites) & Dest.SiteID %in% as.character(graph.contactp.listSites))
 
   # Exclude site to site distances, where the distance is zero
-  riverDistance.table.noZeros = riverDistance.table[riverDistance.table$Total_Length > 0,]
+  riverDistance.table.noZeros <- riverDistance.table[riverDistance.table$Total_Length > 0,]
 
-  riverDistance.table.noZeros$calcProb = (0.005 * ((40 - (riverDistance.table.noZeros$Total_Length / 1000))/39))
-  riverDistance.table.noZeros$calcProb = ifelse(riverDistance.table.noZeros$calcProb < 0, 0, riverDistance.table.noZeros$calcProb)
+  riverDistance.table.noZeros$calcProb <- (0.005 * ((40 - (riverDistance.table.noZeros$Total_Length / 1000))/39))
+  riverDistance.table.noZeros$calcProb <- ifelse(riverDistance.table.noZeros$calcProb < 0, 0, riverDistance.table.noZeros$calcProb)
 
 
   # Simplify table, including only the siteID
-  riverDistance.table.noZeros.edgeList = riverDistance.table.noZeros[,c('Origin.SiteID','Dest.SiteID')]
+  riverDistance.table.noZeros.edgeList <- riverDistance.table.noZeros[,c('Origin.SiteID','Dest.SiteID')]
 
   # Express each site as a factor
   # Levels are assigned based on the site's position within the original adjacency matrix
@@ -37,14 +37,14 @@ CreateRiverDistanceMatrix = function(riverDistance.fileName, graph.contactp.obje
            levels = graph.contactp.listSites)
 
   # Create an edge list, where each site is expressed in terms of it's position, within the original adjacency matrix
-  riverDistance.table.noZeros.edgeList$Origin.Matrix.Pos =
+  riverDistance.table.noZeros.edgeList$Origin.Matrix.Pos <-
     as.numeric(riverDistance.table.noZeros.edgeList$Origin.SiteID)
 
-  riverDistance.table.noZeros.edgeList$Dest.Matrix.Pos =
+  riverDistance.table.noZeros.edgeList$Dest.Matrix.Pos <-
     as.numeric(riverDistance.table.noZeros.edgeList$Dest.SiteID)
 
   # Create an empty matrix, to store distances between sites, along the river
-  riverDistance.matrix = Matrix(data = 0,
+  riverDistance.matrix <- Matrix(data = 0,
                                 nrow = contactp.length,
                                 ncol = contactp.length,
                                 dimnames = list(as.character(graph.contactp.listSites),
@@ -52,9 +52,9 @@ CreateRiverDistanceMatrix = function(riverDistance.fileName, graph.contactp.obje
 
   # Identify positions within the matrix which correspond to contacts, and identify the contacts with '1'
   riverDistance.matrix[cbind(riverDistance.table.noZeros.edgeList$Origin.Matrix.Pos,
-                             riverDistance.table.noZeros.edgeList$Dest.Matrix.Pos)] = riverDistance.table.noZeros$calcProb
+                             riverDistance.table.noZeros.edgeList$Dest.Matrix.Pos)] <- riverDistance.table.noZeros$calcProb
 
-  riverDistance.matrix = as(riverDistance.matrix, 'dgTMatrix')
+  riverDistance.matrix <- as(riverDistance.matrix, 'dgTMatrix')
 
   return(list(riverDistance.table.noZeros, riverDistance.matrix))
 }
