dbe3a3e0f4b7ba734d084c9f14d1619e4e3e0f16
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 09:11:31 +0100

    rename variable graph.siteType as site_type in CreateFarmVector()

--- R/ListFarmingSites.R
+++ R/ListFarmingSites.R
@@ -1,9 +1,9 @@
 CreateFarmVector <- function(graph) {
   # extract site type from sites in connectivity matrix
-  graph.siteType <- igraph::V(graph)$type
+  site_type <- igraph::V(graph)$type
 
   # if site type is "Farm" return 1 (else zero)
-  farm_vector <- as.numeric(graph.siteType == "Farm")
+  farm_vector <- as.numeric(site_type == "Farm")
 
   # return binary (1/0) vector indicating farms in all sites
   return(farm_vector)
