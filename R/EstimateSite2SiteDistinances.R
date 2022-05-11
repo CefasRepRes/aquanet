4422b25d3dc742fd2ce6348d822e493f79ba5840
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 10:39:00 +0100

    rename variable ListSiteLocations.withCatchment.distance.reordered as site_catchment_distances_order in CreateDistanceMatrix()

--- R/EstimateSite2SiteDistinances.R
+++ R/EstimateSite2SiteDistinances.R
@@ -13,18 +13,18 @@ CreateDistanceMatrix <- function(graph, filename_site_catchments) {
 
   # Reorder matrix, so that it is in the same order as the contact matrix
   site_order <- igraph::get.vertex.attribute(graph = graph, name = "siteID", index = igraph::V(graph))
-  ListSiteLocations.withCatchment.distance.reordered <- site_catchments_distances[site_order, site_order]
+  site_catchment_distances_order <- site_catchments_distances[site_order, site_order]
 
   # Exclude self-loops and ignore any distances longer than 5000m
-  ListSiteLocations.withCatchment.distance.reordered[cbind(site_order,site_order)] <- 0
-  ListSiteLocations.withCatchment.distance.reordered[ListSiteLocations.withCatchment.distance.reordered > 5000] <- 0
+  site_catchment_distances_order[cbind(site_order,site_order)] <- 0
+  site_catchment_distances_order[site_catchment_distances_order > 5000] <- 0
 
   # Calculate probability of transmission, based on distance
-  ListSiteLocations.withCatchment.probability.reordered <- 0.005 * exp(-(ListSiteLocations.withCatchment.distance.reordered)^2 * 0.000001)
+  ListSiteLocations.withCatchment.probability.reordered <- 0.005 * exp(-(site_catchment_distances_order)^2 * 0.000001)
   ListSiteLocations.withCatchment.probability.reordered <- ifelse(ListSiteLocations.withCatchment.probability.reordered < 0, 0, ListSiteLocations.withCatchment.probability.reordered)
   ListSiteLocations.withCatchment.probability.reordered <- ifelse(ListSiteLocations.withCatchment.probability.reordered == 0.005, 0, ListSiteLocations.withCatchment.probability.reordered)
 
   ListSiteLocations.withCatchment.probability.reordered <- methods::as(ListSiteLocations.withCatchment.probability.reordered, "dgTMatrix")
 
-  return(list(ListSiteLocations.withCatchment.distance.reordered, ListSiteLocations.withCatchment.probability.reordered, site_catchments))
+  return(list(site_catchment_distances_order, ListSiteLocations.withCatchment.probability.reordered, site_catchments))
 }
