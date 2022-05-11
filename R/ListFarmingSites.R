2008235276fff3493cd874d35b6729ae4f89e483
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 09:09:38 +0100

    add comments to CreateFarmVector()

--- R/ListFarmingSites.R
+++ R/ListFarmingSites.R
@@ -1,6 +1,10 @@
 CreateFarmVector <- function(graph) {
+  # extract site type from sites in connectivity matrix
   graph.siteType <- igraph::V(graph)$type
+
+  # if site type is "Farm" return 1 (else zero)
   farm_vector <- as.numeric(graph.siteType == "Farm")
 
+  # return binary (1/0) vector indicating farms in all sites
   return(farm_vector)
 }
