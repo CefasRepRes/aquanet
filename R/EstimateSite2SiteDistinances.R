f70389acd9f8293c6bb87dd77233fade3754fa2f
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 10:04:45 +0100

    remove reduntant code line to define siteLocationsWithCatchment.filename in CreateDistanceMatrix()

--- R/EstimateSite2SiteDistinances.R
+++ R/EstimateSite2SiteDistinances.R
@@ -1,7 +1,7 @@
 CreateDistanceMatrix <- function(graph, siteLocationsWithCatchment.fileName, ListModelSetupParameters) {
   # Define the British National Grid Referencing System, using Proj4 notation
   britishNationalGrid <- '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs'
-  siteLocationsWithCatchment.fileName <- siteLocationsWithCatchmentDuplicatesRemoved.fileName
+
   # Import the list of site locations, and assign the correct spatial projection system
   ListSiteLocations.withCatchment <- read.csv(siteLocationsWithCatchment.fileName, header = TRUE)
   coordinates(ListSiteLocations.withCatchment) <- c('easting','northing')
