76e742ad8a1fc51fcf031edd7f5bd9c7af7b52b3
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 11:07:58 +0100

    update comments in CreateDistanceMatrix()

--- R/EstimateSite2SiteDistinances.R
+++ R/EstimateSite2SiteDistinances.R
@@ -1,30 +1,31 @@
 CreateDistanceMatrix <- function(graph, filename_site_catchments) {
-  # Define the British National Grid Referencing System, using Proj4 notation
+  # define the British National Grid Referencing System, using Proj4 notation
   britishNationalGrid <- '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs'
 
-  # Import the list of site locations, and assign the correct spatial projection system
+  # import the list of site locations, and assign the correct spatial projection system
   site_catchments <- read.csv(filename_site_catchments, header = TRUE)
   sp::coordinates(site_catchments) <- c('easting', 'northing')
   sp::proj4string(site_catchments) <- sp::CRS(britishNationalGrid)
 
-  # Create a distance matrix
+  # create a distance matrix (assign correct col and row names)
   site_catchments_distances <- sp::spDists(site_catchments)
   dimnames(site_catchments_distances) <- list(site_catchments@data$siteID, site_catchments@data$siteID)
 
-  # Reorder matrix, so that it is in the same order as the contact matrix
+  # reorder distance matrix, so that it is in the same order as the contact matrix
   site_order <- igraph::get.vertex.attribute(graph = graph, name = "siteID", index = igraph::V(graph))
   site_catchment_distances_order <- site_catchments_distances[site_order, site_order]
 
-  # Exclude self-loops and ignore any distances longer than 5000m
-  site_catchment_distances_order[cbind(site_order,site_order)] <- 0
+  # exclude self-loops and ignore any distances longer than 5000 m
+  site_catchment_distances_order[cbind(site_order, site_order)] <- 0
   site_catchment_distances_order[site_catchment_distances_order > 5000] <- 0
 
-  # Calculate probability of transmission, based on distance
+  # calculate probability of transmission, based on distance, if less than 0 or equal to 0.005 reassign as 0
   ListSiteLocations.withCatchment.probability.reordered <- 0.005 * exp(-(site_catchment_distances_order)^2 * 0.000001)
   ListSiteLocations.withCatchment.probability.reordered <- ifelse(ListSiteLocations.withCatchment.probability.reordered < 0, 0, ListSiteLocations.withCatchment.probability.reordered)
   ListSiteLocations.withCatchment.probability.reordered <- ifelse(ListSiteLocations.withCatchment.probability.reordered == 0.005, 0, ListSiteLocations.withCatchment.probability.reordered)
-
   ListSiteLocations.withCatchment.probability.reordered <- methods::as(ListSiteLocations.withCatchment.probability.reordered, "dgTMatrix")
 
+  # return list containing (1) site to site distances, (2) probability of transmission by distance,
+  # and (3) data frame of site catchment information.
   return(list(site_catchment_distances_order, ListSiteLocations.withCatchment.probability.reordered, site_catchments))
 }
