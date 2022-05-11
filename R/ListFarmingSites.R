ba0a08bad2d72a7e8651d98069a9cf9382a3a7f8
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-11 09:03:04 +0100

    change = to assignment operater <- in CreateFarmVector

--- R/ListFarmingSites.R
+++ R/ListFarmingSites.R
@@ -1,6 +1,6 @@
-CreateFarmVector = function(graph_full) {
-  graph.siteType = V(graph_full)$type
-  farm_vector = as.numeric(graph.siteType == "Farm")
+CreateFarmVector <- function(graph_full) {
+  graph.siteType <- V(graph_full)$type
+  farm_vector <- as.numeric(graph.siteType == "Farm")
 
   return(farm_vector)
 }
