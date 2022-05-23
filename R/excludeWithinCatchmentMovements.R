6ecdcfdb7991ba6df856f46ab9dc9a231ea3c047
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-23 16:22:52 +0100

    update comments in excludeWithinCatchmentMovements()

--- R/excludeWithinCatchmentMovements.R
+++ R/excludeWithinCatchmentMovements.R
@@ -1,4 +1,5 @@
 excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, spmatrix_risk_contacts, catchment_movements) {
+  # TODO: contactp input not define
   # TODO: replace list numbers with named elements
   # extract elements from list
   spmatrix_sites_catchment <- catchment_movements[[1]]
@@ -7,50 +8,64 @@ excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, spmatr
   matrix_contacts_exclude <- catchment_movements[[4]]
   site_control_type <- catchment_movements[[5]]
 
-  # Identify catchments placed under control, based on the list of sites currently under control
+  # create matrix of catchments (rows) under control (col 1) by multiplying the sites by whether movements are restricted
   catchments_controlled <- t(spmatrix_sites_catchment) %*% movement.restrictions.bySite
+
+  # determine number of catchments under control
   n_catchments_controlled <- sum(catchments_controlled > 0)
 
-  # If the same catchments are under control as the last time the function was called, skip several steps
+  # if the catchments under control are NOT the same as last time step:
   if (!all(catchments_controlled@x == catchments_controlled_prev@x)) {
-    # Lookup a list of all of the sites contained within the controlled catchments
+    # return vector of sites contained within the controlled catchments and correct those over 1
     secondary.controlled.sites <- as.vector(spmatrix_sites_catchment %*% catchments_controlled)
     secondary.controlled.sites[secondary.controlled.sites > 1] <- 1
 
-    # List all of the contacts made by sites within controlled catchments
+    # create contact probability matrix for sites within controlled catchments
+    # sites outside controlled catchments have p = 0 else 1
     contacts.by.controlledSites <- contactp * secondary.controlled.sites
     contacts.by.controlledSites[contacts.by.controlledSites > 0] <- 1
 
-    # Store the list of sites that are under catchment level controls
+    # reassign the secondary controlled sites element with sites under catchment controls in this time step
     catchment_movements[[6]] <- secondary.controlled.sites
 
+    # if the site control type is 1 or 2
     if (site_control_type %in% c(0,1)) {
-      # List all of the contacts made within controlled catchments
+      # create matrix of all contacts made within controlled catchments
       contacts.withinCatchment.by.controlledSites <- contacts.by.controlledSites * lgmatrix_catch_catch
 
-      # Identify all of the contacts made outside of controlled catchments
+      # create matrix of all contacts made outside of controlled catchments
       matrix_contacts_exclude <- contacts.by.controlledSites - contacts.withinCatchment.by.controlledSites
     }
 
+    # if the site control type is 1 (allow movements within or between infected catchments)
     if (site_control_type == 1) {
-      # Identify contacts made to other sites within controlled catchments
+      # create matrix of contacts made to other sites within controlled catchments
       contacts.between.controlled.catchments <- t(contacts.by.controlledSites) * secondary.controlled.sites
       contacts.between.controlled.catchments <- t(contacts.between.controlled.catchments)
-      # Exclude within catchment movements, from the list of contacts made to other sites within controlled catchments
+
+      # exclude within catchment movements from the matrix of contacts made to other sites within controlled catchments
       contacts.between.controlled.catchments <- contacts.between.controlled.catchments - contacts.withinCatchment.by.controlledSites
-      # Identify all of the contacts made outside of the infection area, rather than outside of each individual catchment
+
+      # create matrix of all contacts made outside of the infection area (rather than outside of each individual catchment)
       matrix_contacts_exclude <- matrix_contacts_exclude - contacts.between.controlled.catchments
+
+    # else if the site control type is 2 (allow no movements by any site within an infected catchment)
     } else if (site_control_type == 2) {
+      # assign contact probability matrix for sites within controlled catchments as matrix_contacts_exclude
       matrix_contacts_exclude <- contacts.by.controlledSites
     }
   }
 
+  # create matrix of contacts to remove and remove from input matrix of risk contacts
   atriskcontacts.toremove <- spmatrix_risk_contacts * matrix_contacts_exclude
   spmatrix_risk_contacts <- spmatrix_risk_contacts - atriskcontacts.toremove
 
+  # reassign new catchment control information (catchment, contacts to exclude and number of catchments)
   catchment_movements[[3]] <- catchments_controlled
   catchment_movements[[4]] <- matrix_contacts_exclude
   catchment_movements[[7]] <- n_catchments_controlled
 
+  # return list containing (1) sparse matrix of risk contacts (excluding within catchment movements
+  # depending on site control measures) and (2) catchment_movements list with updated controlled catchments
   return(list(spmatrix_risk_contacts, catchment_movements))
 }
