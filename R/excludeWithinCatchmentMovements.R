6b028fb29b1d913683b3458979379ef13212fa28
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-23 11:42:43 +0100

    rename withinCatchmentMovements.objects to catchment_movements in excludeithinCatchmentMovements

--- R/excludeWithinCatchmentMovements.R
+++ R/excludeWithinCatchmentMovements.R
@@ -1,9 +1,10 @@
-excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, atriskcontacts, withinCatchmentMovements.objects) {
-  graph.catchment2site.matrix2 <- withinCatchmentMovements.objects[[1]]
-  graph.withinCatchmentEdges.matrix <- withinCatchmentMovements.objects[[2]]
-  controlled.catchments.previous <- withinCatchmentMovements.objects[[3]]
-  listContacts.exclude <- withinCatchmentMovements.objects[[4]]
-  associatedSiteControlType <- withinCatchmentMovements.objects[[5]]
+excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, atriskcontacts, catchment_movements) {
+  # extract elements from list
+  graph.catchment2site.matrix2 <- catchment_movements[[1]]
+  graph.withinCatchmentEdges.matrix <- catchment_movements[[2]]
+  controlled.catchments.previous <- catchment_movements[[3]]
+  listContacts.exclude <- catchment_movements[[4]]
+  associatedSiteControlType <- catchment_movements[[5]]
 
   # Identify catchments placed under control, based on the list of sites currently under control
   controlled.catchments <- t(graph.catchment2site.matrix2) %*% movement.restrictions.bySite
@@ -20,7 +21,7 @@ excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, atrisk
     contacts.by.controlledSites[contacts.by.controlledSites > 0] <- 1
 
     # Store the list of sites that are under catchment level controls
-    withinCatchmentMovements.objects[[6]] <- secondary.controlled.sites
+    catchment_movements[[6]] <- secondary.controlled.sites
 
     if (associatedSiteControlType %in% c(0,1)) {
       # List all of the contacts made within controlled catchments
@@ -46,9 +47,9 @@ excludeWithinCatchmentMovements <- function(movement.restrictions.bySite, atrisk
   atriskcontacts.toremove <- atriskcontacts * listContacts.exclude
   atriskcontacts <- atriskcontacts - atriskcontacts.toremove
 
-  withinCatchmentMovements.objects[[3]] <- controlled.catchments
-  withinCatchmentMovements.objects[[4]] <- listContacts.exclude
-  withinCatchmentMovements.objects[[7]] <- no.controlled.catchments
+  catchment_movements[[3]] <- controlled.catchments
+  catchment_movements[[4]] <- listContacts.exclude
+  catchment_movements[[7]] <- no.controlled.catchments
 
-  return(list(atriskcontacts, withinCatchmentMovements.objects))
+  return(list(atriskcontacts, catchment_movements))
 }
