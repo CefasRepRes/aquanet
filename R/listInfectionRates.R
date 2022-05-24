9e5d7079e582ffb7f471ab124378e30649fbf2f0
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-24 17:10:42 +0100

    overhaul listInfectionRates to reduce repetitive code and apply sensible variable names

--- R/listInfectionRates.R
+++ R/listInfectionRates.R
@@ -1,32 +1,28 @@
-listInfectionRates <- function(spmatrix_risk_contacts, state_vector, trans.type) {
+listInfectionRates <- function(spmatrix_risk_contacts, state_vector, trans_type) {
   # convert 'dgCMatrix' to 'dgTMatrix'
   spmatrix_risk_contacts <- as(t(spmatrix_risk_contacts), "dgTMatrix")
 
   # get matrix value coordinates (i, j) and values (x)
   # NOTE: 0-based coordinates (most R objects have 1-based coordinates)
-  i <- spmatrix_risk_contacts@i
-  j <- spmatrix_risk_contacts@j
-  x <- spmatrix_risk_contacts@x
+  slots <- c("i", "j", "x")
+  slots_risk_contacts <- lapply(setNames(slots, slots), FUN = function(x) { slot(spmatrix_risk_contacts, x) })
 
   # create a logical vector stating whether x is non-zero
-  nonEmpty.logical <- x != 0
+  non_zero <- slots_risk_contacts[["x"]] != 0
 
   # extract coordinates (i, j) and values (x) for at risk contact probabilities which are non-zero
-  i <- i[nonEmpty.logical]
-  j <- j[nonEmpty.logical]
-  x <- x[nonEmpty.logical]
+  slots_risk_contacts <- lapply(setNames(slots, slots), FUN = function(x) { slots_risk_contacts[[x]][non_zero]})
 
   # create logical vector of edges with potential to infect susceptible sites NOTE: i + 1 as 0-based coordinates
-  edges.potential.infect.logical <- state_vector[i + 1] == 0
+  edge_susceptible <- state_vector[slots_risk_contacts[["i"]] + 1] == 0
 
   # subset risk contacts leading to susceptible sites
-  state.pos <- i[edges.potential.infect.logical]
-  source.infection <- j[edges.potential.infect.logical]
-  state.rate <- x[edges.potential.infect.logical]
+  contacts <- lapply(setNames(slots, c("position", "source", "rate")),
+                     FUN = function(x) { slots_risk_contacts[[x]][edge_susceptible]})
 
   # determine number of contacts
-  state.no <- length(state.rate)
-  state.rate.type <- rep.int(trans.type, times = state.no)
+  n_contacts <- length(contacts[["rate"]])
+  contacts_rate_type <- rep.int(0, times = n_contacts)
 
-  return(list(state.rate.type, state.pos, state.rate, source.infection, state.no))
+  return(list(contacts_rate_type, contacts[["position"]], contacts[["rate"]], contacts[["source"]], n_contacts))
 }
