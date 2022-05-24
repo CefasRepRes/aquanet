a7890d08e91200304ea232bda979c9011cea160d
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-24 17:05:08 +0100

    edit comments in listInfectionRates()

--- R/listInfectionRates.R
+++ R/listInfectionRates.R
@@ -1,29 +1,32 @@
 listInfectionRates <- function(spmatrix_risk_contacts, state_vector, trans.type) {
-  # convert matrix to 'dgTMatrix'
-  spmatrix_risk_contacts <- as(t(spmatrix_risk_contacts), 'dgTMatrix')
+  # convert 'dgCMatrix' to 'dgTMatrix'
+  spmatrix_risk_contacts <- as(t(spmatrix_risk_contacts), "dgTMatrix")
 
-  # 'i' and 'j' vectors are coordinates, which are 0-based coordinates (most R objects have 1-based coordinates)
-  # Extract each value's coordinates, and save them into seperate objects
+  # get matrix value coordinates (i, j) and values (x)
+  # NOTE: 0-based coordinates (most R objects have 1-based coordinates)
   i <- spmatrix_risk_contacts@i
   j <- spmatrix_risk_contacts@j
   x <- spmatrix_risk_contacts@x
 
-  # Create a logical vector testing whether x is non-zero
+  # create a logical vector stating whether x is non-zero
   nonEmpty.logical <- x != 0
 
-  # Extract values and the associated coordinates for objects which are non-zero
+  # extract coordinates (i, j) and values (x) for at risk contact probabilities which are non-zero
   i <- i[nonEmpty.logical]
   j <- j[nonEmpty.logical]
   x <- x[nonEmpty.logical]
 
-  # Extract edges which have potential to infect susceptable sites
+  # create logical vector of edges with potential to infect susceptible sites NOTE: i + 1 as 0-based coordinates
   edges.potential.infect.logical <- state_vector[i + 1] == 0
 
+  # subset risk contacts leading to susceptible sites
   state.pos <- i[edges.potential.infect.logical]
+  source.infection <- j[edges.potential.infect.logical]
   state.rate <- x[edges.potential.infect.logical]
+
+  # determine number of contacts
   state.no <- length(state.rate)
   state.rate.type <- rep.int(trans.type, times = state.no)
-  source.infection <- j[edges.potential.infect.logical]
 
   return(list(state.rate.type, state.pos, state.rate, source.infection, state.no))
 }
