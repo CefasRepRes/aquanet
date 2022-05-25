67be6d6e2d8691057d27a74495838bbca2e1e83d
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-25 15:52:41 +0100

    add roxygen2 documentation to listInfectionRates()

--- R/listInfectionRates.R
+++ R/listInfectionRates.R
@@ -1,3 +1,62 @@
+#' listInfectionRates
+#'
+#' Extract information from risk contact matrix related to the sites that have the potential to be
+#' infected (i.e. contact probability is greater than 0 and receiving sites are in a susceptible
+#' state `state_vector[site] == 0`). Output information includes receiving siteID, probability of
+#' contact, source siteID, and the number of contacts that pose a risk to receiving sites. A
+#' transition rate vector is also produced for these sites using the `trans_type` input parameter.
+#'
+#' Note: this function is called within `update_rate` and `calcRiverTransmission` of aquanet-mod.
+#'
+#' TODO: CHECK/UPDATE FUNCTION NAMES
+#'
+#' @param spmatrix_risk_contacts  (class dgCMatrix, Matrix package) sparse matrix containing live
+#' fish movements contact probability adapted to identify only contacts between sites that present a
+#'  risk of spread (of e.g. a pathogen). In aquanet-mod movements between sites in the same
+#' catchment are filtered depending on catchment controls input into
+#' `excludeWithinCatchmentMovements()`.
+#'
+#' Source sites that are infected but cannot transport fish off site due to movement restrictions
+#' have their contact probabilities converted to 0 as they cannot form 'at risk' contacts.
+#' Additionally. sites that are uninfected with or without movement restrictions have a probability
+#' of 0. Receiving sites that cannot transport fish on site due to movement restrictions also have
+#' their contact probabilities converted to 0 as they cannot form 'at risk' contacts. At risk
+#' contacts occur between sites that are infected with no restrictions on movement off site and
+#' receiving sites with no restrictions on site.
+#' (Note: initially defined within `update_rate` function of aquanet-mod. In aquanet-mod this is
+#' one of the outputs from excludeWithinCatchmentMovements()).
+#'
+#' @param state_vector (class numeric) numeric binary vector of length number of sites containing
+#' information on whether each site is in an 1 = infected or 0 = susceptible state.
+#' (Note: created within the `simulationCode` function for loop)
+#'
+#' @param trans_type (class numeric) number stating transition type.
+#'
+#' @return (class list) of length 5 containing:
+#' 1. (class numeric) numeric vector of input `trans_type` with length 'number of sites with contact
+#'  probability greater than 0 where the receiving site is in a susceptible state'.
+#'
+#' 2. (class integer) integer vector containing the positions/coordinate x of sites with risk
+#' contact at susceptible sites `(spmatrix_risk_contacts@i)` with length 'number of sites with
+#' contact probability greater than 0 where the receiving site is in a susceptible state'.
+#'
+#' 3. (class numeric) numeric vector containing the probability of risk contact at sites connected
+#' to susceptible sites. `(spmatrix_risk_contacts@x)` with length 'number of sites with contact
+#' probability greater than 0 where the receiving site is in a susceptible state'.
+#'
+#' 4. (class integer) numeric vector containing the positions/coordinate y of sites with risk
+#' contact at susceptible sites. These are the source sites `(spmatrix_risk_contacts@j)` with length
+#'  'number of sites with contact probability greater than 0 where the receiving site is in a
+#'  susceptible state'.
+#'
+#' 5. (class integer) number of sites that have a risk contact probability above 0 where the
+#' receiving site is in a susceptible state.
+#'
+#' @export
+#'
+#' @importFrom stats setNames
+#' @importFrom methods slot
+#'
 listInfectionRates <- function(spmatrix_risk_contacts, state_vector, trans_type) {
   # convert 'dgCMatrix' to 'dgTMatrix'
   spmatrix_risk_contacts <- as(t(spmatrix_risk_contacts), "dgTMatrix")
