255b1ccc6184b1eb1608d9b9f2b4c36b33ca6cf7
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-26 15:14:58 +0100

    clarify comments in combineTransitionRates() function

--- R/combineTransitions.R
+++ R/combineTransitions.R
@@ -1,12 +1,10 @@
 combineTransitionRates <- function(transition.objects, transition.rates) {
-  # Type of transition
+  # concatenate (1) transition type, (2) transition site, (3) transisiton rate and (4) source site
   transition.rates[[1]] <- c(transition.rates[[1]], transition.objects[[1]])
-  # Site subject to transition
   transition.rates[[2]] <- c(transition.rates[[2]], transition.objects[[2]])
-  # Transition rate
   transition.rates[[3]] <- c(transition.rates[[3]], transition.objects[[3]])
-  # Source of disease (in the case of transmissions)
   transition.rates[[4]] <- c(transition.rates[[4]], transition.objects[[4]])
 
+  # return concatenated transition information list
   return(transition.rates)
 }
