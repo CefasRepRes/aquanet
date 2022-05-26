1c7aae487501991a49ae8e0340f9b9f73997f3ef
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-26 16:25:00 +0100

    rename variable state.pos as position in listTransitionRates()

--- R/listTransitionRates.R
+++ R/listTransitionRates.R
@@ -7,13 +7,13 @@ listTransitionRates <- function(run_time_params,
   state.logical <- state_vector == state.match
 
   # get the position and total number of sites in input state specified
-  state.pos <- site.index[state.logical]
-  state.no <- length(state.pos)
+  position <- site.index[state.logical]
+  state.no <- length(position)
 
   # create vector of transition rates, transition rate type, and infection source
   state.rate <- rep(1 / prob, times = state.no)
   rate_type <- rep(trans.type, times = state.no)
   source.infection <- rep(NA, times = state.no)
 
-  return(list(rate_type, state.pos, state.rate, source.infection, state.no))
+  return(list(rate_type, position, state.rate, source.infection, state.no))
 }
