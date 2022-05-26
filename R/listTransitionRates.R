814c061646690ea26c915a4334fe6abf2147ee53
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-26 16:29:27 +0100

    rename input parameter from trans.type to trans_type in listTransititionRates()

--- R/listTransitionRates.R
+++ R/listTransitionRates.R
@@ -1,7 +1,7 @@
 listTransitionRates <- function(run_time_params,
-                                state_vector, trans.type, site.index, state.match) {
+                                state_vector, trans_type, site.index, state.match) {
   # get probability from input parameter file
-  prob <- run_time_params[[trans.type]]
+  prob <- run_time_params[[trans_type]]
 
   # create logical vector of sites in input state specified
   state.logical <- state_vector == state.match
@@ -12,7 +12,7 @@ listTransitionRates <- function(run_time_params,
 
   # create vector of transition rates, transition rate type, and infection source
   rate <- rep(1 / prob, times = n_rates)
-  rate_type <- rep(trans.type, times = n_rates)
+  rate_type <- rep(trans_type, times = n_rates)
   source_site <- rep(NA, times = n_rates)
 
   return(list(rate_type, position, rate, source_site, n_rates))
