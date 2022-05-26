c048e5a15614e1f9e8719367ec112bee63a16e38
BexMillard <rebecca.millard@cefas.co.uk>
2022-05-26 14:22:14 +0100

    format spacing in combineTransitions

--- R/combineTransitions.R
+++ R/combineTransitions.R
@@ -1,12 +1,12 @@
 combineTransitions <- function(transition.objects, transition.rates) {
   # Type of transition
-  transition.rates[[1]] <- c(transition.rates[[1]],transition.objects[[1]])
+  transition.rates[[1]] <- c(transition.rates[[1]], transition.objects[[1]])
   # Site subject to transition
-  transition.rates[[2]] <- c(transition.rates[[2]],transition.objects[[2]])
+  transition.rates[[2]] <- c(transition.rates[[2]], transition.objects[[2]])
   # Transition rate
-  transition.rates[[3]] <- c(transition.rates[[3]],transition.objects[[3]])
+  transition.rates[[3]] <- c(transition.rates[[3]], transition.objects[[3]])
   # Source of disease (in the case of transmissions)
-  transition.rates[[4]] <- c(transition.rates[[4]],transition.objects[[4]])
+  transition.rates[[4]] <- c(transition.rates[[4]], transition.objects[[4]])
 
   return(transition.rates)
 }
