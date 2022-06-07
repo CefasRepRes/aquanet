0aaf07bf23b4c196dbd40513be26c2bb722067fc
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 14:25:00 +0100

    define aquanet:: package function called within calcRiverTransmission()

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,9 +1,11 @@
-calcRiverTransmission <- function(distanceMatrix, state_vector, spread.offSite.prevented, spread.onSite.prevented, trans.type) {
+calcRiverTransmission <- function(distanceMatrix,state_vector, spread.offSite.prevented, spread.onSite.prevented, trans.type) {
   distanceMatrix <- distanceMatrix * (state_vector * !spread.offSite.prevented)
   distanceMatrix <- t(distanceMatrix) * !spread.onSite.prevented
   distanceMatrix <- t(distanceMatrix)
 
-  listInfectionRates.objects <- listInfectionRates(distanceMatrix, state_vector, trans.type)
+  listInfectionRates.objects <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
+                                                                          state_vector = state_vector,
+                                                                          trans_type = trans.type)
 
   return(listInfectionRates.objects)
 }
