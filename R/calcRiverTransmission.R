3e2d0a83694c35e4523545664b45ad4bc2724ead
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 15:43:29 +0100

    format function inputs for readability

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,4 +1,9 @@
-calcRiverTransmission <- function(distanceMatrix, clinical_state_vector, move_restricted_off, move_restricted_on, trans_type) {
+calcRiverTransmission <- function(distanceMatrix,
+                                  clinical_state_vector,
+                                  move_restricted_off,
+                                  move_restricted_on,
+                                  trans_type) {
+
   distanceMatrix <- distanceMatrix * (clinical_state_vector * !move_restricted_off)
   distanceMatrix <- t(distanceMatrix) * !move_restricted_on
   distanceMatrix <- t(distanceMatrix)
