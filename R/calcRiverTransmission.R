b47d5e95e4ed7569e13c98151df0c0f1557f7b63
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 15:55:35 +0100

    rename input variable from distanceMatrix to matrix_river_distances_prob in calcRiverTransmission()

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,14 +1,14 @@
-calcRiverTransmission <- function(distanceMatrix,
+calcRiverTransmission <- function(matrix_river_distances_prob,
                                   clinical_state_vector,
                                   move_restricted_off,
                                   move_restricted_on,
                                   trans_type) {
 
-  distanceMatrix <- distanceMatrix * (clinical_state_vector * !move_restricted_off)
-  distanceMatrix <- t(distanceMatrix) * !move_restricted_on
-  distanceMatrix <- t(distanceMatrix)
+  matrix_river_distances_prob <- matrix_river_distances_prob * (clinical_state_vector * !move_restricted_off)
+  matrix_river_distances_prob <- t(matrix_river_distances_prob) * !move_restricted_on
+  matrix_river_distances_prob <- t(matrix_river_distances_prob)
 
-  list_river_infection_rates <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
+  list_river_infection_rates <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = matrix_river_distances_prob,
                                                                           state_vector = clinical_state_vector,
                                                                           trans_type = trans_type)
 
