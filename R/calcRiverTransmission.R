e0d93af02ca324608b39881a26c98275694a7977
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 14:26:04 +0100

    rename trans.type input vatiable as trans_type in calcRiverTransmission()

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,11 +1,11 @@
-calcRiverTransmission <- function(distanceMatrix,state_vector, spread.offSite.prevented, spread.onSite.prevented, trans.type) {
+calcRiverTransmission <- function(distanceMatrix, state_vector, spread.offSite.prevented, spread.onSite.prevented, trans_type) {
   distanceMatrix <- distanceMatrix * (state_vector * !spread.offSite.prevented)
   distanceMatrix <- t(distanceMatrix) * !spread.onSite.prevented
   distanceMatrix <- t(distanceMatrix)
 
   listInfectionRates.objects <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
                                                                           state_vector = state_vector,
-                                                                          trans_type = trans.type)
+                                                                          trans_type = trans_type)
 
   return(listInfectionRates.objects)
 }
