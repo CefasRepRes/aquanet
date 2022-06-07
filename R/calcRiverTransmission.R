48c80e895baf35b13363c3998d3e40e27f51e4b6
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 15:38:25 +0100

    rename input variable from spread.onSite.prevented to move_restricted_on in calcRiverTransmission()

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,6 +1,6 @@
-calcRiverTransmission <- function(distanceMatrix, clinical_state_vector, move_restricted_off, spread.onSite.prevented, trans_type) {
+calcRiverTransmission <- function(distanceMatrix, clinical_state_vector, move_restricted_off, move_restricted_on, trans_type) {
   distanceMatrix <- distanceMatrix * (clinical_state_vector * !move_restricted_off)
-  distanceMatrix <- t(distanceMatrix) * !spread.onSite.prevented
+  distanceMatrix <- t(distanceMatrix) * !move_restricted_on
   distanceMatrix <- t(distanceMatrix)
 
   listInfectionRates.objects <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
