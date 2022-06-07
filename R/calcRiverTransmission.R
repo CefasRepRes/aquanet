cf4530c2a60a2864c8793603afdb1d412eb3bd45
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-07 14:50:08 +0100

    rename input variable state_vector to clinical_state_vector in calcRiverTransmission()

--- R/calcRiverTransmission.R
+++ R/calcRiverTransmission.R
@@ -1,10 +1,10 @@
-calcRiverTransmission <- function(distanceMatrix, state_vector, spread.offSite.prevented, spread.onSite.prevented, trans_type) {
-  distanceMatrix <- distanceMatrix * (state_vector * !spread.offSite.prevented)
+calcRiverTransmission <- function(distanceMatrix, clinical_state_vector, spread.offSite.prevented, spread.onSite.prevented, trans_type) {
+  distanceMatrix <- distanceMatrix * (clinical_state_vector * !spread.offSite.prevented)
   distanceMatrix <- t(distanceMatrix) * !spread.onSite.prevented
   distanceMatrix <- t(distanceMatrix)
 
   listInfectionRates.objects <- aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
-                                                                          state_vector = state_vector,
+                                                                          state_vector = clinical_state_vector,
                                                                           trans_type = trans_type)
 
   return(listInfectionRates.objects)
