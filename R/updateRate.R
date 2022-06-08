d5d65d37106ce3b2d156224b7c0f2985c2669a34
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-08 17:24:14 +0100

    edit definition of transition.rates and rename to trans_rates in update_rate()

--- R/updateRate.R
+++ R/updateRate.R
@@ -6,7 +6,8 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # 2. sites whose control status is new
   # 3. contacts made out of controlled catchments
 
-  transition.rates <- c(list(NULL),list(NULL),list(NULL),list(NULL))
+  # create empty list for transition rate storage
+  trans_rates <- vector(mode = "list", length = 4)
 
   # When a site has been contact traced it will be subject to movement controls, but
   # will not be culled, released from controls or used to infer which catchments
@@ -43,7 +44,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
 
   # Create an edge list for live fish movements from infected to exposed sites
   susceptable.sites.exposure.rate.objects <- listInfectionRates(atriskcontacts, state_vector, 0)
-  transition.rates <- combineTransitions(susceptable.sites.exposure.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(susceptable.sites.exposure.rate.objects, trans_rates)
 
   ######## An attempt to make separate the parameters between the fisheries and farms
   ######## Here i will calculate the transition from infected to subclinical for the two types of site separately
@@ -54,7 +55,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # State 3 and 2 leads to recovery and latency, respectively
   infected.sites <- state_vector * infected.sites.withRecovery*farm_vector
   infected.sites.recover.rate.objects <- listTransitionRates(infected.sites, 3, site.index, 1)
-  transition.rates <- combineTransitions(infected.sites.recover.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(infected.sites.recover.rate.objects, trans_rates)
   ########
 
   ######## And this second is for the fisheries
@@ -64,7 +65,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # State 3 and 2 leads to recovery and latency, respectively
   infected.sites <- state_vector * infected.sites.withRecovery*!farm_vector
   infected.sites.recover.rate.objects <- listTransitionRates(infected.sites, 2, site.index, 1)
-  transition.rates <- combineTransitions(infected.sites.recover.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(infected.sites.recover.rate.objects, trans_rates)
   ########
 
   ######## This is the original code
@@ -74,7 +75,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # State 3 and 2 leads to recovery and latency, respectively
   #infected.sites <- state_vector * infected.sites.withRecovery
   #infected.sites.recover.rate.objects <- listTransitionRates(infected.sites, 2, site.index, 1)
-  #transition.rates <- combineTransitions(infected.sites.recover.rate.objects, transition.rates)
+  #trans_rates <- combineTransitions(infected.sites.recover.rate.objects, trans_rates)
   ########
 
   # Identify any latent, infected sites
@@ -82,21 +83,21 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # Create a vector with the recovery rate of latent sites
   latent.sites <- as.logical(control_matrix[,6])
   latent.sites.recovery.rate.objects <- listTransitionRates(latent.sites, 5, site.index, 1)
-  transition.rates <- combineTransitions(latent.sites.recovery.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(latent.sites.recovery.rate.objects, trans_rates)
 
   # Identify sites that are fallow, and infected
   # Create a vector showing the position of sites that are fallow and infected
   # Create a vector showing the rate at which fallow sites are disinfected
   fallow.infected.sites <- state_vector * (control_matrix[,4] + control_matrix[,5])
   fallow.infected.sites.rate.disinfection <- listTransitionRates(fallow.infected.sites, 1, site.index, 1)
-  transition.rates <- combineTransitions(fallow.infected.sites.rate.disinfection, transition.rates)
+  trans_rates <- combineTransitions(fallow.infected.sites.rate.disinfection, trans_rates)
 
   # Identify sites which have been contact traced
   # Create a vector showing the position of sites which have been contact traced
   # Create a vector showing the rate at which contact traced sites will be tested
   contact.traced.sites <- control_matrix[,7]
   contact.traced.sites.rate.testing <- listTransitionRates(contact.traced.sites, 12, site.index, 1)
-  transition.rates <- combineTransitions(contact.traced.sites.rate.testing, transition.rates)
+  trans_rates <- combineTransitions(contact.traced.sites.rate.testing, trans_rates)
 
   ########
   # Identify sites that are under surveillance or management
@@ -107,7 +108,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   #surveillance.sites <- as.logical(control_matrix[,c(2,3,4,5)] %*% rep(1,4))
   #surveillance.sites <- ifelse(surveillance.sites >0, 1, 0)
   #surveillance.sites.rate <- listTransitionRates(surveillance.sites, 7, site.index, 1)
-  #transition.rates <- combineTransitions(surveillance.sites.rate, transition.rates)
+  #trans_rates <- combineTransitions(surveillance.sites.rate, trans_rates)
   ########
 
   # Identify sites that can become controlled
@@ -115,27 +116,27 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # Create a vector with the control rate of infected sites
   infected.sites.notControlled <- control_matrix[,1]
   infected.sites.control.rate.objects <- listTransitionRates(infected.sites.notControlled, 6, site.index, 1)
-  transition.rates <- combineTransitions(infected.sites.control.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(infected.sites.control.rate.objects, trans_rates)
 
   if (winter == FALSE) {
     # Identify any latent, infected sites
     # Create a vector showing the position of latent sites
     # Create a vector with the rate at which sites revert to active expression of disease
     latent.sites.secondOutbreak <- listTransitionRates(latent.sites, 4, site.index, 1)
-    transition.rates <- combineTransitions(latent.sites.secondOutbreak, transition.rates)
+    trans_rates <- combineTransitions(latent.sites.secondOutbreak, trans_rates)
 
 
     # Calculate the probability of a contact occuring downstream of an outbreak, through the river network
     graph.riverDownstream.objects <- graph.riverDistance.objects[[1]]
     riverDownstream.matrix <- graph.riverDownstream.objects[[2]]
     susceptable.sites.exposure.byRiver.downstream.objects <- calcRiverTransmission(riverDownstream.matrix, clinical.vector, spread.offSite.prevented, spread.onSite.prevented, 10)
-    transition.rates <- combineTransitions(susceptable.sites.exposure.byRiver.downstream.objects, transition.rates)
+    trans_rates <- combineTransitions(susceptable.sites.exposure.byRiver.downstream.objects, trans_rates)
 
     ########
     ######## Calculate the probability of a contact occuring due to local fomite transmission
     fomite.matrix <- graph.estimateSiteDistances.objects[[2]]
     susceptable.sites.exposure.byFomites.objects <- calcRiverTransmission(fomite.matrix, clinical.vector, spread.offSite.prevented, spread.onSite.prevented, 14)
-    transition.rates <- combineTransitions(susceptable.sites.exposure.byFomites.objects, transition.rates)
+    trans_rates <- combineTransitions(susceptable.sites.exposure.byFomites.objects, trans_rates)
     ########
 
 
@@ -143,7 +144,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
     #Exclude contacts from sites that can not perticipate in such a mechanism of transmition
     if (sum(!state_vector & !spread.onSite.prevented) != 0) {
       spill.over.objects <- calcRandomSpillover(clinical.vector, spread.offSite.prevented, spread.onSite.prevented, 11)
-      transition.rates <- combineTransitions(spill.over.objects, transition.rates)
+      trans_rates <- combineTransitions(spill.over.objects, trans_rates)
     }
   }
 
@@ -152,7 +153,7 @@ update_rate <- function(state_vector, control_matrix, withinCatchmentMovements.o
   # Create a vector with the rate at which sites become fallow
   controlled.farms <- movement.restrictions.allSite * culling_vector
   controlled.sites.fallow.rate.objects <- listTransitionRates(controlled.farms, 9, site.index, 1)
-  transition.rates <- combineTransitions(controlled.sites.fallow.rate.objects, transition.rates)
+  trans_rates <- combineTransitions(controlled.sites.fallow.rate.objects, trans_rates)
 
-  return(list(transition.rates, withinCatchmentMovements.objects, movement.restrictions.bySite))
+  return(list(trans_rates, withinCatchmentMovements.objects, movement.restrictions.bySite))
 }
