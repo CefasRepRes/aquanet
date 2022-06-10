28bf2c7509e9e32586941495061f2b7c4af79956
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-10 16:12:49 +0100

    rename input parameter site_distances as site_distances_prob in update_rate()

--- R/updateRate.R
+++ R/updateRate.R
@@ -6,7 +6,7 @@ update_rate <- function(state_vector,
                         withinCatchmentMovements.objects,
                         movements_prob,
                         river_prob,
-                        site_distances,
+                        site_distances_prob,
                         run_time_params,
                         non_peak_season) {
 
@@ -165,7 +165,7 @@ update_rate <- function(state_vector,
                                                      trans_type = 10)
 
     # Rate 10: probability of a contact occurring due to local fomite transmission
-    contacts_fomite <- aquanet::calcRiverTransmission(matrix_river_distances_prob = site_distances[[2]],
+    contacts_fomite <- aquanet::calcRiverTransmission(matrix_river_distances_prob = site_distances_prob[[2]],
                                                       clinical_state_vector = clinical_vector,
                                                       spread_restricted_off = spread_prevented_off,
                                                       spread_restricted_on = spread_prevented_on,
