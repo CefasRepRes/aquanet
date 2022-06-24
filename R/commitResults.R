654ecea8862e5fe1079f34c46a7caa518fe94f8c
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-24 16:03:12 +0100

    add comments to commitResults()

--- R/commitResults.R
+++ R/commitResults.R
@@ -14,16 +14,19 @@ commitResults <- function(allStates.table,
   allStates.matrix <- as(object = as.matrix(allStates.table[((n_states + 1):(n_states + n_sites)), ]),
                          Class = "dgTMatrix")
 
+  # create data frame of simulation site states at each iteration
   sim_states <- data.frame(siteID = as.integer(site_indices[(allStates.matrix@i + 1)] + 1),
                            state = as.integer(allStates.matrix@x),
                            timeID = as.integer(allStates.matrix@j + ((save_num - 1) * commit_int)),
                            simNo = as.integer(allStates.table[3, ])[allStates.matrix@j + 1])
 
+  # create data frame of simulation times
   sim_times <- data.frame(timeID = as.integer(iteration_vector + ((save_num - 1) * commit_int)),
                           simNo = as.integer(allStates.table[3, ])[iteration_vector],
                           tdiff = as.numeric(allStates.table.t[1, ])[iteration_vector],
                           t = as.numeric(allStates.table.t[2, ])[iteration_vector])
 
+  # save simulation site states and simulation times
   save(sim_states,
        sim_times,
        file = paste(filepath_results,
