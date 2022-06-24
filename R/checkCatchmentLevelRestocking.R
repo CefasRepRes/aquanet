d9d0cc8369f252934129525c87c60b6f43009a3f
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-24 11:39:12 +0100

    rename input variable from graph.catchment2site.matrix2 as spmatrix_sites_catchments in checkCatchmentLevelRestocking()

--- R/checkCatchmentLevelRestocking.R
+++ R/checkCatchmentLevelRestocking.R
@@ -1,5 +1,5 @@
 checkCatchmentLevelRestocking <- function(control_matrix,
-                                          graph.catchment2site.matrix2,
+                                          spmatrix_sites_catchment,
                                           n_catchments) {
   # create binary vector of sites in fallow or post-fallow (awaiting restock) state
   # TODO: columns names instead of column numbers
@@ -7,8 +7,8 @@ checkCatchmentLevelRestocking <- function(control_matrix,
   sites_post_fallow <- control_matrix[ , 5]
 
   # create vector with number of sites in fallow or post-fallow state in each catchment
-  catchment_n_sites_fallow <- as.vector(Matrix::t(graph.catchment2site.matrix2) %*% sites_fallow)
-  catchment_n_sites_post_fallow <- as.vector(Matrix::t(graph.catchment2site.matrix2) %*% sites_post_fallow)
+  catchment_n_sites_fallow <- as.vector(Matrix::t(spmatrix_sites_catchment) %*% sites_fallow)
+  catchment_n_sites_post_fallow <- as.vector(Matrix::t(spmatrix_sites_catchment) %*% sites_post_fallow)
 
   # create logical vector of catchments with more than one fallow or post-fallow site
   catchment_with_fallow <- catchment_n_sites_fallow > 0
