a22362f7e71ff6b09502ebec22c57a58fc83f95f
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-24 12:00:22 +0100

    add roxygen2 documentation to checkCatchmentLevelRestocking() function

--- R/checkCatchmentLevelRestocking.R
+++ R/checkCatchmentLevelRestocking.R
@@ -1,3 +1,35 @@
+#' checkCatchmentLevelRestocking
+#'
+#' Function takes control matrix stating which sites are in a fallow state and which are in a post-
+#' fallow state (ready for restocking). This information is combined with the siteID-to-catchment
+#' matrix to calculate the number of sites in a fallow or post-fallow state within each catchment.
+#' This information is then used to determine which catchments contain only sites in a post-fallow
+#' state that are ready for restocking and to determine which catchments contain sites in a fallow
+#' state.
+#'
+#' @param control_matrix (class matrix) matrix containing 7 columns depicting different control
+#' states and rows (of length number of sites) depicting whether each sites is 1 = in the specified
+#' control state or 0 = not in the specified control state.
+#'
+#' @param spmatrix_sites_catchment (class dgCMatrix, Matrix package) sparse matrix containing site
+#' to catchment summary.
+#' Note: this is created with the `aquanet::createCatchmentToSiteMatrix()` function.
+#'
+#' @param n_catchments (class integer) number of catchments.
+#'
+#' @return (class list) of length 3 containing:
+#' 1. (class matrix) input `control_matrix` containing 7 columns depicting different control
+#' states and rows (of length number of sites) depicting whether each sites is 1 = in the specified
+#' control state or 0 = not in the specified control state. This matrix is unedited and output to
+#' keep a record of the input data used to generate these catchment restocking data.
+#' 2. (class logical) vector of length number of catchments (`n_catchments`) depicting whether
+#' catchments contain fallow sites.
+#' 3. (class logical) vector of length number of catchments (`n_catchments`) depicting whether
+#' catchments contain only post-fallow sites ready for restocking with no fallow sites.
+#'
+#' @export
+#'
+#' @importFrom Matrix t
 checkCatchmentLevelRestocking <- function(control_matrix,
                                           spmatrix_sites_catchment,
                                           n_catchments) {
