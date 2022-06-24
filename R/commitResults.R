1d3c3ee10bbb34ea28e7229c764136e15005b517
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-24 15:57:42 +0100

    rename input parameter iterationID.vector as iteration_vector in commitResults()

--- R/commitResults.R
+++ R/commitResults.R
@@ -4,11 +4,11 @@ commitResults <- function(allStates.table,
                           n_sites,
                           site_indices,
                           commit_int,
+                          iteration_vector,
                           filepath_results,
                           batch_num,
                           simulation_num,
-                          save_num,
-                          iterationID.vector) {
+                          save_num) {
 
   # create empty dgTMatrix to record site state at each step within the specified commit interval
   allStates.matrix <- as(object = as.matrix(allStates.table[((n_states + 1):(n_states + n_sites)), ]),
@@ -19,10 +19,10 @@ commitResults <- function(allStates.table,
                            timeID = as.integer(allStates.matrix@j + ((save_num - 1) * commit_int)),
                            simNo = as.integer(allStates.table[3, ])[allStates.matrix@j + 1])
 
-  sim_times <- data.frame(timeID = as.integer(iterationID.vector + ((save_num - 1) * commit_int)),
-                          simNo = as.integer(allStates.table[3, ])[iterationID.vector],
-                          tdiff = as.numeric(allStates.table.t[1, ])[iterationID.vector],
-                          t = as.numeric(allStates.table.t[2, ])[iterationID.vector])
+  sim_times <- data.frame(timeID = as.integer(iteration_vector + ((save_num - 1) * commit_int)),
+                          simNo = as.integer(allStates.table[3, ])[iteration_vector],
+                          tdiff = as.numeric(allStates.table.t[1, ])[iteration_vector],
+                          t = as.numeric(allStates.table.t[2, ])[iteration_vector])
 
   save(sim_states,
        sim_times,
