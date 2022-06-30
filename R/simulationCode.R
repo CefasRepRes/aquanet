1bea798dcd7df793e94025788e998d9497069dd2
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-30 15:34:46 +0100

    remove listInfectionRates() function and replace with aquanet::listRatesSusceptibleRiskContacts() in simulationCode()

--- R/simulationCode.R
+++ R/simulationCode.R
@@ -79,36 +79,6 @@ simulationCode = function(graph.contactp.objects, runs, tmax, batchNo, ListRunTi
     return(list(state.rate.type, state.pos, state.rate, source.infection, state.no))
   }
 
-  listInfectionRates = function(distanceMatrix, state_vector, trans.type) {
-    # Convert matrix to 'dgTMatrix'
-    distanceMatrix = as(t(distanceMatrix), 'dgTMatrix')
-
-    # 'i' and 'j' vectors are coordinates, which are 0-based coordinates (most R objects have 1-based coordinates)
-    # Extract each value's coordinates, and save them into seperate objects
-    i = distanceMatrix@i
-    j = distanceMatrix@j
-    x = distanceMatrix@x
-
-    # Create a logical vector testing whether x is non-zero
-    nonEmpty.logical = x != 0
-
-    # Extract values and the associated coordinates for objects which are non-zero
-    i = i[nonEmpty.logical]
-    j = j[nonEmpty.logical]
-    x = x[nonEmpty.logical]
-
-    # Extract edges which have potential to infect susceptable sites
-    edges.potential.infect.logical = state_vector[i + 1] == 0
-
-    state.pos = i[edges.potential.infect.logical]
-    state.rate = x[edges.potential.infect.logical]
-    state.no = length(state.rate)
-    state.rate.type = rep.int(trans.type, times = state.no)
-    source.infection = j[edges.potential.infect.logical]
-
-    return(list(state.rate.type, state.pos, state.rate, source.infection, state.no))
-  }
-
   calcRandomSpillover = function(state_vector, spread.offSite.prevented, spread.onSite.prevented, trans.type) {
     spread.onSite.Index = site.index[!state_vector & !spread.onSite.prevented]
     spread.offSite.Index = site.index[state_vector & !spread.offSite.prevented]
@@ -137,7 +107,9 @@ simulationCode = function(graph.contactp.objects, runs, tmax, batchNo, ListRunTi
     distanceMatrix = t(distanceMatrix) * !spread.onSite.prevented
     distanceMatrix = t(distanceMatrix)
 
-    listInfectionRates.objects = listInfectionRates(distanceMatrix, state_vector, trans.type)
+    listInfectionRates.objects = aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = distanceMatrix,
+                                                                           state_vector = state_vector,
+                                                                           trans_type = trans.type)
 
     return(listInfectionRates.objects)
   }
@@ -243,7 +215,9 @@ simulationCode = function(graph.contactp.objects, runs, tmax, batchNo, ListRunTi
     withinCatchmentMovements.objects = withinCatchmentMovements.out.objects[[2]]
 
     # Create an edge list for live fish movements from infected to exposed sites
-    susceptable.sites.exposure.rate.objects = listInfectionRates(atriskcontacts, state_vector, 0)
+    susceptable.sites.exposure.rate.objects = aquanet::listRatesSusceptibleRiskContacts(spmatrix_risk_contacts = atriskcontacts,
+                                                                                        state_vector = state_vector,
+                                                                                        trans_type = 0)
     transition.rates = combineTransitions(susceptable.sites.exposure.rate.objects, transition.rates)
 
     ######## An attempt to make separate the parameters between the fisheries and farms
