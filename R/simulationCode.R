757bb42a677445a9f8a95b1d82f15c7a713bbd30
BexMillard <rebecca.millard@cefas.co.uk>
2022-06-30 15:27:09 +0100

    remove excludeWithinCatchmentMovements() function definition within simulationCode()

--- R/simulationCode.R
+++ R/simulationCode.R
@@ -56,61 +56,6 @@ simulationCode = function(graph.contactp.objects, runs, tmax, batchNo, ListRunTi
   # Create a vector to record transition times, for diagnostic purposes
   record_transition_times = c()
 
-  excludeWithinCatchmentMovements = function(movement.restrictions.bySite, atriskcontacts, withinCatchmentMovements.objects) {
-    graph.catchment2site.matrix2 = withinCatchmentMovements.objects[[1]]
-    graph.withinCatchmentEdges.matrix = withinCatchmentMovements.objects[[2]]
-    controlled.catchments.previous = withinCatchmentMovements.objects[[3]]
-    listContacts.exclude = withinCatchmentMovements.objects[[4]]
-    associatedSiteControlType = withinCatchmentMovements.objects[[5]]
-
-    # Identify catchments placed under control, based on the list of sites currently under control
-    controlled.catchments = t(graph.catchment2site.matrix2) %*% movement.restrictions.bySite
-    no.controlled.catchments = sum(controlled.catchments > 0)
-
-    # If the same catchments are under control as the last time the function was called, skip several steps
-    if ((!all(controlled.catchments@x == controlled.catchments.previous@x)) == TRUE) {
-      # Lookup a list of all of the sites contained within the controlled catchments
-      secondary.controlled.sites = as.vector(graph.catchment2site.matrix2 %*% controlled.catchments)
-      secondary.controlled.sites[secondary.controlled.sites > 1] = 1
-
-      # List all of the contacts made by sites within controlled catchments
-      contacts.by.controlledSites = contactp * secondary.controlled.sites
-      contacts.by.controlledSites[contacts.by.controlledSites > 0] = 1
-
-      # Store the list of sites that are under catchment level controls
-      withinCatchmentMovements.objects[[6]] = secondary.controlled.sites
-
-      if (associatedSiteControlType %in% c(0,1)) {
-        # List all of the contacts made within controlled catchments
-        contacts.withinCatchment.by.controlledSites = contacts.by.controlledSites * graph.withinCatchmentEdges.matrix
-
-        # Identify all of the contacts made outside of controlled catchments
-        listContacts.exclude = contacts.by.controlledSites - contacts.withinCatchment.by.controlledSites
-      }
-
-      if (associatedSiteControlType == 1) {
-        # Identify contacts made to other sites within controlled catchments
-        contacts.between.controlled.catchments = t(contacts.by.controlledSites) * secondary.controlled.sites
-        contacts.between.controlled.catchments = t(contacts.between.controlled.catchments)
-        # Exclude within catchment movements, from the list of contacts made to other sites within controlled catchments
-        contacts.between.controlled.catchments = contacts.between.controlled.catchments - contacts.withinCatchment.by.controlledSites
-        # Identify all of the contacts made outside of the infection area, rather than outside of each individual catchment
-        listContacts.exclude = listContacts.exclude - contacts.between.controlled.catchments
-      } else if (associatedSiteControlType == 2) {
-        listContacts.exclude = contacts.by.controlledSites
-      }
-    }
-
-    atriskcontacts.toremove = atriskcontacts * listContacts.exclude
-    atriskcontacts = atriskcontacts - atriskcontacts.toremove
-
-    withinCatchmentMovements.objects[[3]] = controlled.catchments
-    withinCatchmentMovements.objects[[4]] = listContacts.exclude
-    withinCatchmentMovements.objects[[7]] = no.controlled.catchments
-
-    return(list(atriskcontacts, withinCatchmentMovements.objects))
-  }
-
   listTransitionRates = function(state_vector, trans.type, site.index, state.match) {
 
 
@@ -290,7 +235,10 @@ simulationCode = function(graph.contactp.objects, runs, tmax, batchNo, ListRunTi
     atriskcontacts = t(atriskcontacts) * !transport.onSite.prevented
     atriskcontacts = t(atriskcontacts)
 
-    withinCatchmentMovements.out.objects = excludeWithinCatchmentMovements(movement.restrictions.bySite, atriskcontacts, withinCatchmentMovements.objects)
+    withinCatchmentMovements.out.objects = aquanet::excludeWithinCatchmentMovements(move_restricted_sites = movement.restrictions.bySite,
+                                                                                    spmatrix_risk_contacts = atriskcontacts,
+                                                                                    catchment_movements = withinCatchmentMovements.objects,
+                                                                                    matrix_movements_prob = contactp)
     atriskcontacts = withinCatchmentMovements.out.objects[[1]]
     withinCatchmentMovements.objects = withinCatchmentMovements.out.objects[[2]]
 
