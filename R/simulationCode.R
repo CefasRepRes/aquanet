1202de1a6b806dffe8f37dbb70a6e7526d287bae
BexMillard <rebecca.millard@cefas.co.uk>
2022-07-01 12:18:35 +0100

    fix: * for & symbol in farm selection if statement of simulationCode()

--- R/simulationCode.R
+++ R/simulationCode.R
@@ -182,7 +182,7 @@ simulationCode <- function(createContactProbabilityMatrix_out,
     for(d in 0:length(farm_vector)) {
       d <- d + 1
       value <- farm_vector[d] * d
-      if (value > 0 * !stats::is.na(value)) {
+      if (value > 0 & !stats::is.na(value)) {
       farm_select <- c(farm_select, value)
       }
     }
