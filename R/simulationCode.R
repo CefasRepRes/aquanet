2c7cf9fbfc0ba6daabd00b6da301d6c310a4c142
BexMillard <rebecca.millard@cefas.co.uk>
2022-07-01 17:10:56 +0100

    remove definition of variables combfarm.vector and farmcombinedstates.total that are not used elsewhere in simulationCode()

--- R/simulationCode.R
+++ R/simulationCode.R
@@ -234,8 +234,6 @@ simulationCode <- function(createContactProbabilityMatrix_out,
 
       # farms: summarise number of farms in each of the n_states ----
       farm_states_vector <- state_vector * farm_vector
-      combfarm.vector <- farm_vector * sites_states_vector
-      farmcombinedstates.total <- tabulate(combfarm.vector, nbins = n_states)
       farm_states_cumulative <- (farm_states_vector | farm_states_cumulative)
 
       # fisheries: summarise number of fisheries in each of the n_states ----
