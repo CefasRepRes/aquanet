20c217affe8ffdf9d10dff9de89cb0d5bfad7968
BexMillard <rebecca.millard@cefas.co.uk>
2022-07-07 11:03:41 +0100

    specify aquanet::simulationCode inputs in runSimulations()

--- R/runSimulations.R
+++ R/runSimulations.R
@@ -11,7 +11,9 @@ runSimulations <- function(graph.contactp.objects,
                            locationSaveResults,
                            seedNo,
                            n_initial_infections,
-                           tmax) {
+                           tmax,
+                           n_states,
+                           non_peak_season_length) {
 
   # list files ending in .RData in the results directory
   files <- list.files(path = locationSaveResults,
@@ -43,19 +45,21 @@ runSimulations <- function(graph.contactp.objects,
   # run simulation in parallel
   allruns <-
     foreach::foreach(batchNo = 1:n_cores, .combine = c) %dorng% aquanet::simulationCode(
-      graph.contactp.objects,
-      n_sims_per_job,
-      tmax,
-      batchNo,
-      ListRunTimeParameters,
-      graph.withinCatchmentEdges.objects,
-      graph.catchment2Site.objects,
-      graph.riverDistance.objects,
-      graph.estimateSiteDistances.objects,
-      farm_vector,
-      associatedSiteControlType,
-      locationSaveResults,
-      n_initial_infections
+      runs = n_sims_per_job,
+      tmax = tmax,
+      batch_num = batchNo,
+      run_time_params = ListRunTimeParameters,
+      non_peak_season_length = non_peak_season_length,
+      createContactProbabilityMatrix_out = graph.contactp.objects,
+      createWithinCatchmentEdges_out = graph.withinCatchmentEdges.objects,
+      createCatchmentToSiteMatrix_out = graph.catchment2Site.objects,
+      createRiverDistanceProbabilityMatrix_out_list = graph.riverDistance.objects,
+      createDistanceMatrix_out = graph.estimateSiteDistances.objects,
+      farm_vector = farm_vector,
+      n_states = n_states,
+      n_initial_infections = n_initial_infections,
+      type_catchment_controls = associatedSiteControlType,
+      filepath_results = locationSaveResults
     )
 
   # shut down set of copies of R running in parallel communicating over sockets
