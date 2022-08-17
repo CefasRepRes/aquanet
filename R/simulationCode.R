ba49d39371dbc763b5fb0ac8be7e91b576f457b6
BexMillard <rebecca.millard@cefas.co.uk>
2022-08-17 10:52:13 +0100

    remove empty.vector column from allStates.table

--- R/simulationCode.R
+++ R/simulationCode.R
@@ -146,8 +146,8 @@ simulationCode <- function(runs,
   # TODO: do these need "empty.vector" columns?
   allStates.table <- stats::setNames(data.table::data.table(matrix(0,
                                                                    nrow = 3 + n_sites + n_states, # 3 batch_num, k, sim_num
-                                                                   ncol = commit_int + 1)),
-                                     c("empty.vector", as.character(iteration_vector)))
+                                                                   ncol = commit_int)),
+                                     as.character(iteration_vector))
 
   allStates.table.t <- stats::setNames(data.table::data.table(matrix(0,
                                                                      nrow = 2,
