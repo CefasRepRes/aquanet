fbb888a9f7e8d7f72044d761ba1d7d6c1bb49704
Sarah <sarah.alewijnse@cefas.co.uk>
2022-08-23 11:29:06 +0100

    Added global variables

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+utils::globalVariables(c(".",
+                         "batch_num",
+                         "cumulative_no_infected_sites",
+                         "max_inf",
+                         "max_t",
+                         "sim_no",
+                         "summaryStates.table"))
