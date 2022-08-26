ed7bb0f22ca21076efb39bc9102895fea7a37ccc
BexMillard <rebecca.millard@cefas.co.uk>
2022-08-26 15:44:18 +0100

    format inputs

--- R/listRatesSusceptibleRiskContacts.R
+++ R/listRatesSusceptibleRiskContacts.R
@@ -1,20 +1,21 @@
 #' listRatesSusceptibleRiskContacts
 #'
+#' This function lists transition rates for sites that can be infected via the LFM disease
+#' transmission route of AquaNet-Mod (see details).
+#'
 #' Extract information from risk contact matrix related to the sites that have the potential to be
 #' infected (i.e. contact probability is greater than 0 and receiving sites are in a susceptible
 #' state `state_vector[site] == 0`). Output information includes receiving siteID, probability of
 #' contact, source siteID, and the number of contacts that pose a risk to receiving sites. A
 #' transition rate vector is also produced for these sites using the `trans_type` input parameter.
 #'
-#' Note: this function is called within `update_rate` and `calcRiverTransmission` of aquanet-mod.
-#'
-#' TODO: CHECK/UPDATE FUNCTION NAMES
+#' Note: this function is called within `aquanet::updateRates` and `aquanet::calcRiverTransmission`
+#' of AquaNet-Mod.
 #'
 #' @param spmatrix_risk_contacts  (class dgCMatrix, Matrix package) sparse matrix containing live
 #' fish movements contact probability adapted to identify only contacts between sites that present a
-#'  risk of spread (of e.g. a pathogen). In aquanet-mod movements between sites in the same
-#' catchment are filtered depending on catchment controls input into
-#' `excludeWithinCatchmentMovements()`.
+#'  risk of spread of pathogen. In AquaNet-Mod movements between sites in the same catchment are
+#' filtered depending on catchment controls input into `aquanet::excludeWithinCatchmentMovements()`.
 #'
 #' Source sites that are infected but cannot transport fish off site due to movement restrictions
 #' have their contact probabilities converted to 0 as they cannot form 'at risk' contacts.
@@ -22,13 +23,13 @@
 #' of 0. Receiving sites that cannot transport fish on site due to movement restrictions also have
 #' their contact probabilities converted to 0 as they cannot form 'at risk' contacts. At risk
 #' contacts occur between sites that are infected with no restrictions on movement off site and
-#' receiving sites with no restrictions on site.
-#' (Note: initially defined within `update_rate` function of aquanet-mod. In aquanet-mod this is
-#' one of the outputs from excludeWithinCatchmentMovements()).
+#' receiving sites with no restrictions on site. (Note: initially defined within
+#' `aquanet::updateRates` function of AquaNet-Mod. In AquaNet-Mod this is one of the outputs
+#' from `aquanet::excludeWithinCatchmentMovements()`).
 #'
 #' @param state_vector (class numeric) numeric binary vector of length number of sites containing
 #' information on whether each site is in an 1 = infected or 0 = susceptible state.
-#' (Note: created within the `simulationCode` function for loop)
+#' (Note: created within the `aquanet::simulationCode` function for loop).
 #'
 #' @param trans_type (class numeric) number stating transition type.
 #'
@@ -57,7 +58,9 @@
 #' @importFrom stats setNames
 #' @importFrom methods slot
 #'
-listRatesSusceptibleRiskContacts <- function(spmatrix_risk_contacts, state_vector, trans_type) {
+listRatesSusceptibleRiskContacts <- function(spmatrix_risk_contacts,
+                                             state_vector,
+                                             trans_type) {
   # convert 'dgCMatrix' to 'dgTMatrix'
   spmatrix_risk_contacts <- as(t(spmatrix_risk_contacts), "dgTMatrix")
 
@@ -83,5 +86,9 @@ listRatesSusceptibleRiskContacts <- function(spmatrix_risk_contacts, state_vecto
   n_contacts <- length(contacts[["rate"]])
   contacts_rate_type <- rep.int(trans_type, times = n_contacts)
 
-  return(list(contacts_rate_type, contacts[["position"]], contacts[["rate"]], contacts[["source"]], n_contacts))
+  return(list(contacts_rate_type,
+              contacts[["position"]],
+              contacts[["rate"]],
+              contacts[["source"]],
+              n_contacts))
 }
